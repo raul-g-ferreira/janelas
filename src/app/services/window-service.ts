2e98478164f5cfb8c08d20d8a94ab19fd4862686
raul-g-ferreira <raul.gferreira7@gmail.com>
2026-05-06 12:06:25 -0300

    Resolvendo janela fora dos limites quando maximizada ou redimensionada

--- src/app/services/window-service.ts
+++ src/app/services/window-service.ts
@@ -80,7 +80,7 @@ export class WindowService {
         ...w,
         x: 0,
         y: 0,
-        width: window.innerWidth,
+        width: window.innerWidth - 100,
         height: window.innerHeight,
         isMaximized: true
       } : w
